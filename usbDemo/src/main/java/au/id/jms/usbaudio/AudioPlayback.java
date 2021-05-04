49bfb90210d58421c163fa41e2ee0163acbc20e7
Jimmy Hu <Jimmy.Hu@avermedia.com>
2021-05-04 10:26:07 +0800

    UVCCamera port sample.

--- usbDemo/src/main/java/au/id/jms/usbaudio/AudioPlayback.java
+++ usbDemo/src/main/java/au/id/jms/usbaudio/AudioPlayback.java
@@ -5,30 +5,95 @@ import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.util.Log;
 
+/**
+ * Reference from https://github.com/shenki/usbaudio-android-demo
+ * <p>
+ * A playback callback from native code to play UAC.
+ */
 public class AudioPlayback {
-	private static final String TAG = "AudioPlayback";
-	
-	private static final int SAMPLE_RATE_HZ = 48000;
-	
-	private static AudioTrack track = null;
-		
-	public static void setup() {
-		Log.i(TAG, "Audio Playback");
-		
-		int bufSize = AudioTrack.getMinBufferSize(SAMPLE_RATE_HZ, 
-				AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
-		Log.d(TAG, "Buf size: " + bufSize);
-		
-		track = new AudioTrack(AudioManager.STREAM_MUSIC,
-				SAMPLE_RATE_HZ,
-				AudioFormat.CHANNEL_OUT_STEREO,
-				AudioFormat.ENCODING_PCM_16BIT,
-				bufSize,
-				AudioTrack.MODE_STREAM);
-		track.play();
-	}
-	
-	public static void write(byte[] decodedAudio) {
-		track.write(decodedAudio, 0, decodedAudio.length);
-	}
+    private static final String TAG = "AudioPlayback";
+
+    private static final int SAMPLE_RATE_HZ = 48000;
+
+    /**
+     * Callback interface that UAC has receive data or other things.
+     */
+    public interface Callback {
+        /**
+         * called when a new audio data coming
+         *
+         * @param data decoded audio data
+         */
+        void onData(byte[] data);
+    }
+
+    private static AudioTrack track = null;
+    private static Callback callback = null;
+
+    /**
+     * Setup audio track playback
+     *
+     * @param cb optional callback
+     */
+    public static void setup(Callback cb) {
+        Log.i(TAG, "Audio Playback");
+
+        int bufSize = AudioTrack.getMinBufferSize(SAMPLE_RATE_HZ,
+                AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
+        Log.d(TAG, "Buffer size: " + bufSize);
+
+        release(); // make sure the old track is released
+        track = new AudioTrack(AudioManager.STREAM_MUSIC,
+                SAMPLE_RATE_HZ,
+                AudioFormat.CHANNEL_OUT_STEREO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                bufSize,
+                AudioTrack.MODE_STREAM);
+        // track.play();
+        callback = cb;
+    }
+
+    /**
+     * Start audio playback
+     */
+    public static void start() {
+        // Log.d(TAG, "start AudioTrack " + track);
+        if (track != null) track.play();
+    }
+
+    /**
+     * Stop audio playback
+     */
+    public static void stop() {
+        if (track != null) track.stop();
+        // Log.d(TAG, "stop AudioTrack " + track);
+    }
+
+    /**
+     * Release audio track
+     */
+    public static void release() {
+        callback = null;
+        if (track != null) {
+            // track.stop();
+            track.release();
+            track = null;
+        }
+    }
+
+    /**
+     * A callback entry for native code.
+     *
+     * @param decodedAudio decoded audio data
+     */
+    public static void write(byte[] decodedAudio) {
+        // Log.d(TAG, String.format("size = %d", decodedAudio.length));
+        try {
+            if (track != null) track.write(decodedAudio, 0, decodedAudio.length);
+            if (callback != null) callback.onData(decodedAudio);
+        } catch (Exception e) {
+            Log.e(TAG, "write: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
--- usbDemo/src/main/java/au/id/jms/usbaudio/UsbAudio.java
+++ usbDemo/src/main/java/au/id/jms/usbaudio/UsbAudio.java
@@ -1,14 +1,73 @@
 package au.id.jms.usbaudio;
 
+import com.serenegiant.usb.USBMonitor;
+
+/**
+ * Reference from https://github.com/shenki/usbaudio-android-demo
+ * <p>
+ * USB Audio native
+ */
 public class UsbAudio {
     static {
         System.loadLibrary("usbaudio");
     }
 
-    public native boolean setup(int fd);
-    public native void close();
+    /**
+     * Setup USB audio device.
+     *
+     * @param vid vendor id
+     * @param pid product id
+     * @param fd  file descriptor
+     * @param bus bus
+     * @param dev device
+     * @return true if setup successfully.
+     */
+    public native boolean setup(int vid, int pid, int fd, int bus, int dev);
+
+    /**
+     * Close USB audio device.
+     *
+     * @return true if device closed.
+     */
+    public native boolean close();
+
+    /**
+     * Loop usb event dispatcher.
+     */
     public native void loop();
+
+    /**
+     * Start playing.
+     *
+     * @return true if play procedure success
+     */
+    public native boolean start();
+
+    /**
+     * Stop playing.
+     *
+     * @return true if stop procedure success
+     */
     public native boolean stop();
+
+    /**
+     * Get this session statistics.
+     *
+     * @return
+     */
     public native int measure();
 
+    /**
+     * Open USB audio device.
+     *
+     * @param ctrlBlock USB control block
+     */
+    public void open(final USBMonitor.UsbControlBlock ctrlBlock) {
+        if (ctrlBlock == null) {
+            throw new NullPointerException("no UsbControlBlock");
+        }
+
+        setup(ctrlBlock.getVenderId(), ctrlBlock.getProductId(), ctrlBlock.getFileDescriptor(),
+                ctrlBlock.getBusNum(), ctrlBlock.getDevNum());
+    }
 }
